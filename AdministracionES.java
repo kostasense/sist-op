8be3b80a1dc19d7fe0c428ff90ece6a06f7f11cc
codinavia <olivas.grecia29@gmail.com>
2025-04-06 14:17:08 -0700

    idk

--- AdministracionES.java
+++ AdministracionES.java
@@ -5,16 +5,14 @@ public class AdministracionES {
         LinkedList<Peticion> p1 = new LinkedList<>(p.getPeticiones());
         LinkedList<Peticion> p2 = new LinkedList<>();
 
-        int sectorActual = 0, distancia = 0, rendimiento = 0, direccion = 0;
+        int sectorActual = 0, distancia = 0, rendimiento = 0, direccion = 1;
 
-        while (!p1.isEmpty() && !p2.isEmpty()) {
-
-            while (!p1.isEmpty()) {
-                if (direccion == 0) Collections.sort(p1);
-                else p1.sort(Collections.reverseOrder());
-
-                direccion = Math.abs(cabeza-sectorActual);
-            }
-        }
+        /*while (!p1.isEmpty() && !p2.isEmpty()) {
+            p1.sort((a, b)-> {
+                return direccion == 1
+                    ? Integer.compare(a.getSector(), b.getSector())
+                    : Integer.compare(b.getSector(), a.getSector());
+            });
+        }*/
     }
 }
