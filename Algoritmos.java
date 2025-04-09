7067ca54ad90d2a071f22eb871e43d408c2f4ee8
Angel Vizcaino <leonard_viz@hotmail.com>
2025-04-09 12:14:17 -0700

    random proceso agregado

--- Algoritmos.java
+++ Algoritmos.java
@@ -335,6 +335,9 @@ public class Algoritmos {
         System.out.println("\nTabla inicial de procesos:");
         Planificador.pcb(procesos);
 
+        int aleatorio = rd.nextInt(procesos.size()) + 1;
+        System.out.println("Random generado: " + aleatorio);
+
         Stack<Proceso> pila = new Stack<>();
         ArrayList<Integer> terminados = new ArrayList<>();
 
@@ -393,6 +396,12 @@ public class Algoritmos {
                                   """);
                     Planificador.informe(procesos, terminados, pila);
                     return;
+                }
+
+                if (p.getIdProceso() == aleatorio) {
+                    Proceso pnuevo = new Proceso(procesos.get(procesos.size() - 1).getIdProceso() + 1, rd.nextInt(8) + 3, (rd.nextInt(2) == 1 ? "Listo" : "Bloqueado"));
+                    procesos.add(pnuevo);
+                    System.out.println("Se crea nuevo proceso: " + pnuevo.getIdProceso());
                 }
             }
         }
@@ -663,6 +672,9 @@ public class Algoritmos {
         Planificador.pcb(procesos);
 
         int prioridadActual = procesos.get(0).getPrioridad();
+
+        int aleatorio = rd.nextInt(procesos.size()) + 1;
+        System.out.println("Random generado: " + aleatorio);
         
         while (sim > 0 && !procesos.isEmpty()) {
 
@@ -730,6 +742,14 @@ public class Algoritmos {
                         }
                         
                         Planificador.pcb(procesos);
+                    }
+
+                    if (p.getIdProceso() == aleatorio) {
+                        Proceso pnuevo = new Proceso(20, rd.nextInt(8) + 3, (rd.nextInt(2) == 1 ? "Listo" : "Bloqueado"));
+                            int prioridad = rd.nextInt(4) + 1;
+                            pnuevo.setPrioridad(prioridad);
+                        procesos.add(pnuevo);
+                        System.out.println("Se crea nuevo proceso: " + pnuevo.getIdProceso());
                     }
                 }
             }
